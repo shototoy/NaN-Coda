842bf841a7072bbfd8028bf03367351a54be411e
shototoy <julecyrusarabit@sksu.edu.ph>
2026-03-31 17:51:49 +0800

    ...

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   root: './',
+  publicDir: false,
   build: {
     outDir: 'public',
     emptyOutDir: true,
